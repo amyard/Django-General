5a3d0d3fd1810ba6f700380470116a30aea4d3f0
amyard <maksymturenko@gmail.com>
2019-04-10 23:42:31 +0300

    reset password move to users app

--- ToDo/users/urls.py
+++ ToDo/users/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth import logout
 from django.contrib.auth.views import LogoutView
 
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
 
@@ -15,5 +17,22 @@ urlpatterns = [
     # registration
     path('signup/', signup, name='signup'),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+
+    # reset password
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/actions/password_reset.html',
+                                                                 email_template_name='users/actions/password_reset_email.html',
+                                                                 subject_template_name='users/actions/password_reset_subject.txt',
+                                                                 success_url=reverse_lazy('users:password_reset_done')),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/actions/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/actions/password_reset_confirm.html',
+                                                     success_url=reverse_lazy('users:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/actions/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
