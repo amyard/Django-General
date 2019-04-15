e9afcaa65196686e481da6d192107e3d3a7afc36
amyard <maksymturenko@gmail.com>
2019-04-16 01:01:37 +0300

    app core: seven days view

--- ToDo/core/urls.py
+++ ToDo/core/urls.py
@@ -5,4 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('', MainPageListView.as_view(), name = 'base-view'),
+    path('nextweek', NextSevenDaysView.as_view(), name = 'next-week'),
 ]
--- ToDo/core/views.py
+++ ToDo/core/views.py
@@ -26,3 +26,17 @@ class MainPageListView(LoginRequiredMixin, ListView):
         context['tasks'] = Task.objects.filter(project__user= self.request.user, timestamp=date.today(), status=0)
         context['date'] = date.today()
         return context
+
+
+class NextSevenDaysView(ListView):
+    model = Project
+    template_name = 'core/sevendays.html'
+    start_date = date.today() + timedelta(days=1)
+    end_date = date.today() + timedelta(days=8)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(NextSevenDaysView, self).get_context_data(*args, **kwargs)
+        context['projects'] = self.model.objects.filter(user = self.request.user)
+        context['tasks'] = Task.objects.filter(project__user__username=self.request.user, status=0, timestamp__range=(self.start_date, self.end_date))
+        context['dates'] = [(date.today() + timedelta(days=x)).strftime('%Y-%m-%d') for x in range(1, 8)]
+        return context
