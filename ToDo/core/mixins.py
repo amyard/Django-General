6385c56ff13537f2f7292ec45a08dc927dc86bae
amyard <maksymturenko@gmail.com>
2019-04-16 18:14:10 +0300

    app projects: count tasks/ can ot delete project with tasks

---
+++ ToDo/core/mixins.py
@@ -0,0 +1,16 @@
+from django.views.generic.list import MultipleObjectMixin
+from tasks.models import Task
+from projects.models import Project
+from django.db.models import Count, Q
+from datetime import date
+
+
+class MainPageListMixin(MultipleObjectMixin):
+
+    def get_context_data(self, *args, **kwargs):
+        context = {}
+        context['projects'] = Project.objects.filter(user=self.request.user)
+        context['count_tasks'] = Project.objects.filter(user=self.request.user).annotate(
+            count=Count('project__title', filter=Q(project__status=0) & Q(project__timestamp__gte=date.today()))).values('title', 'count')
+
+        return context
--- ToDo/core/views.py
+++ ToDo/core/views.py
@@ -1,6 +1,7 @@
-# from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.db.models import Count, Q
+from .mixins import MainPageListMixin
 
 from projects.models import Project
 from tasks.models import Task
@@ -8,6 +9,9 @@ from tasks.models import Task
 from datetime import date, timedelta
 
 
+
+
+# from django.contrib.auth.decorators import login_required
 # @login_required(login_url="/accounts/login/")
 # def home(request):
 #     return render(request, 'todo_core/main.html', {'info': 'It\'s working.'})
@@ -15,20 +19,19 @@ from datetime import date, timedelta
 
 
 
-class MainPageListView(LoginRequiredMixin, ListView):
+class MainPageListView(LoginRequiredMixin, ListView, MainPageListMixin):
     model = Project
     template_name = 'core/main.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(MainPageListView, self).get_context_data(*args, **kwargs)
-
-        context['projects'] = self.model.objects.filter(user = self.request.user)
         context['tasks'] = Task.objects.filter(project__user= self.request.user, timestamp=date.today(), status=0)
         context['date'] = date.today()
         return context
 
 
-class NextSevenDaysView(ListView):
+
+class NextSevenDaysView(LoginRequiredMixin, ListView, MainPageListMixin):
     model = Project
     template_name = 'core/sevendays.html'
     start_date = date.today() + timedelta(days=1)
@@ -36,7 +39,6 @@ class NextSevenDaysView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(NextSevenDaysView, self).get_context_data(*args, **kwargs)
-        context['projects'] = self.model.objects.filter(user = self.request.user)
         context['tasks'] = Task.objects.filter(project__user__username=self.request.user, status=0, timestamp__range=(self.start_date, self.end_date))
         context['dates'] = [(date.today() + timedelta(days=x)).strftime('%Y-%m-%d') for x in range(1, 8)]
         return context
