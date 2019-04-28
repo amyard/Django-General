07986cbcc0a39b17f5ec50bdd4ff34e6ea2484b3
amyard <maksymturenko@gmail.com>
2019-04-28 23:07:37 +0300

    first models: category, brand, product

--- eShop/core/admin.py
+++ eShop/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class BrandAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductAdmin)
--- eShop/core/models.py
+++ eShop/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+
+def save_image_path(instance, filename):
+    filename = instance.slug + '.jpg'
+    return f'{instance.category}/{instance.brand}/{filename}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 55, unique = True)
+    slug = models.SlugField(max_length = 55, unique = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length = 55, unique = True)
+    slug = models.SlugField(max_length = 55, unique = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 255, unique = True)
+    slug = models.SlugField(max_length = 255, unique = True)
+    description = models.TextField()
+    image = models.ImageField(upload_to = save_image_path, blank = True, default = 'default.png')
+    price = models.DecimalField(max_digits = 2, decimal_places = 2)
+    available = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
