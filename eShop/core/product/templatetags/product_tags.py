55c6c417c8e4077ad343fbbfa5ceb09cf390a363
amyard <maksymturenko@gmail.com>
2019-05-08 22:32:00 +0300

    создаем слайдеры из продуктов для main page

--- eShop/core/product/templatetags/product_tags.py
+++ eShop/core/product/templatetags/product_tags.py
@@ -7,20 +7,20 @@ register = template.Library()
 
 
 @register.simple_tag
-def show_latest_product(count=4):
+def show_latest_product(count=10):
     return Product.objects.order_by('-id')[:count]
 
 
 @register.simple_tag
-def most_liked_products(count=4):
-    uniq_ids = Like.objects.values('product').annotate(count=Count('product')).order_by('-count')[:4].values_list('product', flat=True)
+def most_liked_products(count=12):
+    uniq_ids = Like.objects.values('product').annotate(count=Count('product')).order_by('-count')[:count].values_list('product', flat=True)
     preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(uniq_ids)])
     res = Product.objects.filter(id__in=uniq_ids).order_by(preserved)
     return res
 
 @register.simple_tag
-def most_commented_products(count=4):
-    uniq_ids = Comment.objects.values('product').annotate(count=Count('product')).order_by('-count')[:4].values_list('product', flat=True)
+def most_commented_products(count=12):
+    uniq_ids = Comment.objects.values('product').annotate(count=Count('product')).order_by('-count')[:count].values_list('product', flat=True)
     preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(uniq_ids)])
     res = Product.objects.filter(id__in=uniq_ids).order_by(preserved)
     return res
